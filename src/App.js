b1f7017e17aacd70367c25a93cec304acdc4afb7
mehedih20 <mehedih20@gmail.com>
2021-10-04 21:50:31 +0600

    improvements v2

--- src/App.js
+++ src/App.js
@@ -19,16 +19,16 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/about">
+          <Route exact path="/about">
             <About />
           </Route>
-          <Route path="/contact">
+          <Route exact path="/contact">
             <Contact />
           </Route>
           <Route exact path="/services">
             <Services />
           </Route>
-          <Route path="/services/course/:name">
+          <Route exact path="/services/course/:name">
             <Service />
           </Route>
           <Route path="*">
